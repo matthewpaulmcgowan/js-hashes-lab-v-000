0f3a5a5d6a62dce0ba9ef1eb2588c062db97a541
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-04-29 16:22:54 -0500

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function addIngredient(recipe,ingredientName,amount){
+  recipe[ingredientName] = amount;
+  return recipe;
+}
+
+function removeIngredient(recipe,ingredientName){
+  delete recipe[ingredientName];
+  return recipe;
+}
+
+function updateIngredient(recipe,ingredientName,newAmount){
+  recipe[ingredientName] = newAmount;
+  return recipe;
+}
+
+function readRecipe(recipe){
+  if (Object.keys(recipe).length > 0){
+    for (var key in recipe){
+      console.log('this recipe calls for ' + recipe[key] + ' of ' + key);
+    }
+  } else {
+    console.log("your recipe is empty");
+  }
+}
